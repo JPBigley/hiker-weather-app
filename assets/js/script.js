92ccddc49d1807ac7e25c217aceab0f0bbeaa3a4
John Bigley <jpbigley@gmail.com>
2022-07-07 19:19:41 -0400

    added weather api functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,26 @@ var searchCityButton = $("#search-city-button");
 
 // Open Weather API Key
 var apiKey = "377d677ec9c51941e2b7d5548195ff39";
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
+// Get the current weather for the city entered
 searchCityButton.on("click", function (event) {
 	event.preventDefault();
-	var searchValue = searchCityInput.val().trim();
-	currentConditionsRequest(searchValue);
-	searchCityInput.val("");
+	var city = searchCityInput.val();
+	var queryUrl = apiUrl + city + "&appid=" + apiKey;
+	$.ajax({
+		url: queryUrl,
+		method: "GET",
+	}).then(function (response) {
+		var cityName = response.name;
+		var temp = response.main.temp;
+		var tempF = Math.floor((temp - 273.15) * 1.8 + 32);
+		var weatherDesc = response.weather[0].description;
+		var weatherIcon = response.weather[0].icon;
+		var weatherIconUrl = "http://openweathermap.org/img/wn/" + weatherIcon + ".png";
+		var weatherResults = $("#weather-results");
+		weatherResults.empty();
+		// append the city name, temperature, weather description, and weather icon to the weather results div
+		weatherResults.append("<p>City: " + cityName + "</p>" + "<p>Temperature: " + tempF + "&#xb0;</p>" + "<p>Conditions: " + weatherDesc + "</p>" + "<p><img src=" + weatherIconUrl + "></p>");
+	});
 });
-
-function getWeather(searchValue) {
-	fetch(apiUrl + "&q=" + searchValue + "&appid=" + apiKey)
-		.then((response) => response.json())
-		.then((data) => console.log(data));
-}
-
-getWeather();
