b23190fd4ba9bf0957d63a845f09087b748abb56
awoods1893 <awoods1983@gmail.com>
2022-07-07 18:59:26 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-var weatherURL = ""
-var parkURL ="https://developer.nps.gov/api/v1/parks?limit=1&api_key=8rhE0zKdQhsoe0lbFkVxuqBk1zLZwzeuwo3D1MXL"
 
-function getParkURL(){
-    fetch('https://developer.nps.gov/api/v1/parks?limit=1&api_key=8rhE0zKdQhsoe0lbFkVxuqBk1zLZwzeuwo3D1MXL')
-   .then (response => response.json())
-   .then(data => {
-    console.log(data.data[0].fullName);
-    console.log(data.data[0].latitude);
-    console.log(data.data[0].longitude);
-   })
-}
 
-    getParkURL();
+  var parkqueryUrl = 'https://developer.nps.gov/api/v1/parks?limit=467&api_key=8rhE0zKdQhsoe0lbFkVxuqBk1zLZwzeuwo3D1MXL&q=<"city: Denve      
+$.ajax({
+    url: parkqueryUrl,
+    method: 'GET',
+
+}).then(function(response){
+    var parkResults = $('#park-results');
+    parkResults.empty();
+    parkResults.append()
+})
+	
