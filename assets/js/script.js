28c65590215c3ae58ee43e215ea4954c7a5618b6
John Bigley <jpbigley@gmail.com>
2022-07-09 10:50:30 -0400

    made weather show data horizontally

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,46 +10,54 @@ searchCityButton.on("click", function (event) {
 	event.preventDefault();
 	var city = searchCityInput.val();
 	var queryUrl = apiUrl + city + "&appid=" + apiKey;
-    var parkqueryUrl = `https://developer.nps.gov/api/v1/parks?limit=467&api_key=8rhE0zKdQhsoe0lbFkVxuqBk1zLZwzeuwo3D1MXL&q="city: ${city}"`;
-	console.log(parkqueryUrl)
-    $.ajax({
+	var parkqueryUrl = `https://developer.nps.gov/api/v1/parks?limit=467&api_key=8rhE0zKdQhsoe0lbFkVxuqBk1zLZwzeuwo3D1MXL&q="city: ${city}"`;
+	console.log(parkqueryUrl);
+	$.ajax({
 		url: queryUrl,
 		method: "GET",
 	}).then(function (response) {
 		var cityName = response.name;
-		var temp = response.main.temp;
-		var tempF = Math.floor((temp - 273.15) * 1.8 + 32);
-		var weatherDesc = response.weather[0].description;
-		var weatherIcon = response.weather[0].icon;
+		var temp = response.main.temp; // temp in Kelvin
+		var tempF = Math.floor((temp - 273.15) * 1.8 + 32); // Convert Kelvin to Fahrenheit
+		var weatherDesc = response.weather[0].description; // Get the weather description
+		var weatherIcon = response.weather[0].icon; // Get the icon code
 		var weatherIconUrl = "http://openweathermap.org/img/wn/" + weatherIcon + ".png";
 		var weatherResults = $("#weather-results");
 		weatherResults.empty();
 		// append the city name, temperature, weather description, and weather icon to the weather results div
-		weatherResults.append("<p>City: " + cityName + "</p>" + "<p>Temperature: " + tempF + "&#xb0;</p>" + "<p>Conditions: " + weatherDesc + "</p>" + "<p><img src=" + weatherIconUrl + "></p>");
-             
-$.ajax({
-    url: parkqueryUrl,
-    method: 'GET',
+		weatherResults.append(
+			"<p><strong>City:</strong> " +
+				cityName +
+				"</p>" +
+				"<p><strong>Temperature:</strong> " +
+				tempF +
+				"&#xb0;</p>" +
+				"<p><strong>Conditions:</strong> " +
+				weatherDesc +
+				"</p>" +
+				"<p><img src=" +
+				weatherIconUrl +
+				"></p>"
+		);
 
-})
-  .then(function (parkResponse){
-       console.log(parkResponse)
-    let i=0;
-    Object.values(parkResponse.data).map((data1)=> {
-      console.log(data1)
-        if (i===0){
-            $('#close-hike').append("<p>Name: " +data1.fullName, "<p>" + "<p>Description:" +data1.description)
-        } else if (i===1){
-            $('#2nd-hike').append("<p>Name: " +data1.fullName, "<p>" + "<p>Description:" +data1.description)
-        }
-        else if (i===2){
-            $('#3-hike"').append("<p>Name: " +data1.fullName, "<p>" + "<p>Description:" +data1.description)
-        }
+		$.ajax({
+			url: parkqueryUrl,
+			method: "GET",
+		}).then(function (parkResponse) {
+			console.log(parkResponse);
+			let i = 0;
+			Object.values(parkResponse.data).map((data1) => {
+				console.log(data1);
+				if (i === 0) {
+					$("#close-hike").append("<p>Name: " + data1.fullName, "<p>" + "<p>Description:" + data1.description);
+				} else if (i === 1) {
+					$("#2nd-hike").append("<p>Name: " + data1.fullName, "<p>" + "<p>Description:" + data1.description);
+				} else if (i === 2) {
+					$('#3-hike"').append("<p>Name: " + data1.fullName, "<p>" + "<p>Description:" + data1.description);
+				}
 
-        i+=1
-    })
-
-})
+				i += 1;
+			});
+		});
 	});
 });
-
